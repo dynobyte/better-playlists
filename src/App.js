97364a654e8d71b814f830e390b2934cb6e90e96
dino soifua <dinopearson801@gmail.com>
2018-07-17 15:33:22 -0600

    added components and styled

--- src/App.js
+++ src/App.js
@@ -2,22 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let defaultTextColor = 'red';
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+class Aggregate extends Component{
+  render(){
+    return (
+      <div style={{...defaultStyle,  width : "40%", display:'inline-block' }}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component{
+  render(){
+    return(
+      <div style={{defaultStyle}}>
+        <img/>
+        <input type="text"/>
+       
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component{
+  render(){
+    return(
+      <div style={ {...defaultStyle , display: 'inline-block', width : '25%'} }>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-
-    let name = "Dino";
-    let green = '#FF1212';
-    let headerStyle = { color : green, "font-size" : "50px"} 
-    
+     
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 style={headerStyle} >Hello {name}! Lets Learn React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
